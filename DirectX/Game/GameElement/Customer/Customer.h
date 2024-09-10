11be30a149c8244a85b8ecf14f21c0cca69c084f
k022g1060@g.neec.ac.jp <k024g6001@g.neec.ac.jp>
2024-09-10 17:08:44 +0900

    お客さんクラスを作成

---
+++ DirectX/Game/GameElement/Customer/Customer.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "GameElement/Object/Object.h"
+
+class Customer
+{
+public:
+	Customer();
+	~Customer();
+
+private:
+
+};
+
+Customer::Customer()
+{
+}
+
+Customer::~Customer()
+{
+}
